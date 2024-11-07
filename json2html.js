87053e8efda68e64c0cc859e6168672839faa02d
Gangadhar Marka <markagangadhar7@gmail.com>
2024-11-07 19:26:49 +0530

    Add indexx

--- json2html.js
+++ json2html.js
@@ -1,10 +1,13 @@
+// json2html.js
+
+// This function accepts an array of objects and converts it into an HTML table.
 export default function json2html(data) {
   // Determine unique keys from the data to create table headers
   const headers = Array.from(new Set(data.flatMap(obj => Object.keys(obj))));
 
   // Start building the HTML table
   let html = '<table data-user="markagangadhar7@gmail.com">';
-  
+
   // Add table headers
   html += '<thead><tr>';
   headers.forEach(header => {
@@ -17,12 +20,13 @@ export default function json2html(data) {
   data.forEach(row => {
     html += '<tr>';
     headers.forEach(header => {
-      html += `<td>${row[header] || ''}</td>`;  // Fill empty cells if key is missing
+      // Fill empty cells if a key is missing in the object
+      html += `<td>${row[header] || ''}</td>`;
     });
     html += '</tr>';
   });
   html += '</tbody></table>';
-  
+
   // Return the complete HTML table as a string
   return html;
 }
